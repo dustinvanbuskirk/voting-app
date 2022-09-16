9a187fab144396dd92a47c11487f787c9abea51c
Dustin Van Buskirk <dev@vanbuskirk.me>
2022-09-16 10:11:31 -0700

    wolverine vs. sabretooth

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "MARVEL")
-option_b = os.getenv('OPTION_B', "DC")
+option_a = os.getenv('OPTION_A', "Wolverine")
+option_b = os.getenv('OPTION_B', "Sabretooth")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
